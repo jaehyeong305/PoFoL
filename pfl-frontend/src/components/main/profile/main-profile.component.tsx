94a8ceae779a12ac0f0c67754941beacf84db8d5
jaehyeong305 <jaehyeong.ha.305@gmail.com>
2023-08-03 11:53:34 +0900

    PFL-13 [WIP] create main career

--- pfl-frontend/src/components/main/profile/main-profile.component.tsx
+++ pfl-frontend/src/components/main/profile/main-profile.component.tsx
@@ -23,14 +23,47 @@ const MainProfileComponent: React.FC = () => {
     };
   }, [scrollContext]);
   
+  const [revealTextVisible, setRevealTextVisible] = useState(false);
+
+  React.useEffect(() => {
+    const revealText = document.querySelector(".main-career");
+
+    const handleScrollReveal = () => {
+      if (revealText) {
+        const revealTextPosition = revealText.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (revealTextPosition < windowHeight * 0.8) {
+          setRevealTextVisible(true);
+        } else {
+          setRevealTextVisible(false);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScrollReveal);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollReveal);
+    };
+    }, []);
+
   return (
-    <div className="scroll-container">
-      <div className="scroll-test">
+    <div className="main-container">
+      <div className="main">
         <img src="images/main-background.png" alt=""></img>
         <img src="images/scroll.png" alt="" className={`plz-scroll ${showScrollIcon ? 'show' : 'hide'}`}></img>
         <div>
-          {/*<span className="discription-name">Jaehyeong Ha</span>*/}
-          {/*<span className="discription-pfl">PortFoLio</span>*/}
+          <span className="discription1">환영합니다.</span>
+            <span className="discription2">소통하는 <span className="position">Front-End Developer</span></span>
+          <span className="discription3"><span className="name">하재형</span> 입니다.</span>
+        </div>
+        <div className={`main-career ${revealTextVisible ? 'show' : ''}`}>
+          <span className="main-title">개발자 경력 총 4년 1개월</span>
+          <span className="main-section1">
+            <img src="/images/bizreach-img.png" alt=""></img>
+            <span>일본 전직(転職)사이트 1위 기업</span>
+          </span>
         </div>
       </div>
     </div>
