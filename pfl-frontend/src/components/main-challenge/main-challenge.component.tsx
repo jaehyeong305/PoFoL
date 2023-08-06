0be4e2701272c20cae309b57480b1a82764454b1
jaehyeong305 <jaehyeong.ha.305@gmail.com>
2023-08-06 16:52:16 +0900

    PFL-13 scrollY to scroll context

--- pfl-frontend/src/components/main-challenge/main-challenge.component.tsx
+++ pfl-frontend/src/components/main-challenge/main-challenge.component.tsx
@@ -1,7 +1,9 @@
+import {useScrollContext} from 'context/scroll.context';
 import React, {useState, useEffect} from 'react';
 import './main-challenge.component.scss';
 
 const MainChallengeComponent: React.FC = () => {
+    const scrollContext = useScrollContext();
     const [isImageFixed, setIsImageFixed] = useState(false);
     const [opacity, setOpacity] = useState(0.7);
     const [textOpacity, setTextOpacity] = useState(1);
@@ -9,27 +11,25 @@ const MainChallengeComponent: React.FC = () => {
 
     useEffect(() => {
         const handleScroll = () => {
-            const scrollY = window.scrollY;
-            
             // NOTE(hajae): 해당 컴포넌트가 스크롤 되었을 때, 해당 이미지를 화면에 고정한다.
-            if (scrollY >= 3750 && scrollY <= 5500) {
+            if (scrollContext >= 3750 && scrollContext <= 5500) {
                 setIsImageFixed(true);
                 // NOTE(hajae): 또한, 해당 컴포넌트를 벗어날 때, 투명도를 높여 없어지는 듯이 표시한다.
-                const opacity = 1 - (scrollY - 5000) / 500;
+                const opacity = 1 - (scrollContext - 5000) / 500;
                 setOpacity(opacity);
             } else {
                 setIsImageFixed(false);
             }
-            
+
             // NOTE(hajae): 해당 컴포넌트를 스크롤 할 때, 텍스트가 아래에서 올라오는 듯이 표시한다.
-            if (scrollY >= 3750 && scrollY <= 4900) {
-                const y = 42 - ((scrollY - 3750) / (4900 - 3750)) * (42 + 65);
+            if (scrollContext >= 3750 && scrollContext <= 4900) {
+                const y = 42 - ((scrollContext - 3750) / (4900 - 3750)) * (42 + 65);
                 setTransformY(y);
             }
-            
+
             // NOTE(hajae): 텍스트가 올라가며 없어질 때, 투명도를 높여 없어지는 듯이 표시한다.
-            if (scrollY >= 4300 && scrollY <= 4700) {
-                const textOpacity = 1 - (scrollY - 4300) / (4700 - 4300);
+            if (scrollContext >= 4300 && scrollContext <= 4700) {
+                const textOpacity = 1 - (scrollContext - 4300) / (4700 - 4300);
                 setTextOpacity(textOpacity);
             }
         };
@@ -38,12 +38,12 @@ const MainChallengeComponent: React.FC = () => {
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
-    }, []);
+    }, [scrollContext]);
 
     return (
         <div className="main-challenge">
             <img src="images/background.png" alt="" className={isImageFixed ? "fixed-image" : ""}
-                style={{opacity: `${opacity}`}}></img>
+                 style={{opacity: `${opacity}`}}></img>
             <div className={`description ${isImageFixed ? "current" : ""}`}>
                 <p className="text-box">
                     <span style={{transform: `translate3d(0px, ${transformY}px, 0px)`, opacity: `${textOpacity}`}}>일본에서 경험을 쌓고</span>
@@ -51,7 +51,8 @@ const MainChallengeComponent: React.FC = () => {
                 </p>
                 <p className="text-box">
                     <span style={{transform: `translate3d(0px, ${transformY}px, 0px)`, opacity: `${textOpacity}`}}>한국에서 새로운 도전을 위해 귀국했습니다.</span>
-                    <span style={{transform: `translate3d(0px, ${transformY}px, 0px)`}}>[TODO] 문구는 나중에 한번 생각해보도록 하자</span>
+                    <span
+                        style={{transform: `translate3d(0px, ${transformY}px, 0px)`}}>[TODO] 문구는 나중에 한번 생각해보도록 하자</span>
                 </p>
             </div>
         </div>
