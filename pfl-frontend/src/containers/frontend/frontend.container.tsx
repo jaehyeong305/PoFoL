8b0c920690336da790b2ef6a643cec121ba16dba
jaehyeong305 <jaehyeong.ha.305@gmail.com>
2023-08-16 19:59:16 +0900

    PFL-22 add frontend-summary-component

--- pfl-frontend/src/containers/frontend/frontend.container.tsx
+++ pfl-frontend/src/containers/frontend/frontend.container.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import HeaderComponent from "ui-component/header/header.component";
 import "./frontend.container.scss";
 import {HEADER_LOGO_ROOT, HEADER_TABS} from "constants/AppConstants";
+import FrontendSummaryComponent from "components/project/frontend-summary/frontend-summary.component";
 
 const FrontendContainer: React.FC = () => {
     return (
         <div className="frontend-container">
             <HeaderComponent rogoImageUrl={HEADER_LOGO_ROOT} headerTabs={HEADER_TABS}/>
+            <FrontendSummaryComponent></FrontendSummaryComponent>
         </div>
     );
 }
